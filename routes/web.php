0c7f1e2a56b6f0d35812e7bf24c0af69735c749b
MrKhairy03 <mubarakhairy10@gamil.com>
2026-01-12 22:16:18 +0700

    chore: initial commit (Trackify: user activity logging & analytics dashboard)

--- routes/web.php
+++ routes/web.php
@@ -17,10 +17,6 @@ Route::get('/dashboard', [DashboardController::class, 'index'])
 
 Route::middleware('auth')->group(function () {
     Route::get('/activities', [ActivitiesController::class, 'index'])->name('activities.index');
-
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
 require __DIR__ . '/auth.php';
